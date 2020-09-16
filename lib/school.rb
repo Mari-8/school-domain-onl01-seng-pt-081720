a0bb4306199ccb0fa32c2df62475516bb7e58677
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-16 01:19:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+class School
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+    
+  end
+  
+  def add_student(name, grade)
+      if @roster[grade] 
+        @roster[grade] << [name].join
+      else 
+        @roster[grade] = [] 
+        @roster[grade] = [name] 
+      end
+  end
+  
+  
+   def grade(grade)
+     @roster[grade]
+   end
+   
+   def sort 
+     @roster.sort_by {|key, value| value}
+     @roster
+   end
+   
+   
+   
+end
